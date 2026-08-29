164dd76fe91be1551ac9d7558224aef40456b4ef
AFumachi <afumachi@gmail.com>
2026-08-29 10:45:23 -0300

    add broker mosquitto

--- 1_N2_Gateway_LoRa/1_Gateway_LoRa_Config_Radio_MQTT_QoS1_Vs4/1_Gateway_LoRa_Config_Radio_MQTT_QoS1_Vs4.ino
+++ 1_N2_Gateway_LoRa/1_Gateway_LoRa_Config_Radio_MQTT_QoS1_Vs4/1_Gateway_LoRa_Config_Radio_MQTT_QoS1_Vs4.ino
@@ -14,7 +14,7 @@
 //                     2 - Configurações MQTT
 // =====================================================================
 const char* MQTT_BROKER   = "broker.hivemq.com";
-//const char* MQTT_BROKER   = "broker.mosquitto.com";
+//const char* MQTT_BROKER   = "test.mosquitto.org";
 const int   MQTT_PORT     = 1883;
 const char* TOPIC_DL      = "mot_lora_mqtt_A2F/gateway/downlink";  // Python → ESP32
 const char* TOPIC_UL      = "mot_lora_mqtt_A2F/gateway/uplink";    // ESP32  → Python
